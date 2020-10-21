90070142856a5c893291ed432aef6eae1d5e65ca
batista12 <50904472+batista12@users.noreply.github.com>
2020-10-20 21:59:46 -0700

    [Manasi]Linked List is created

---
+++ Linkedlist1/src/test/java/com/capgemini/linkedlist1/MyNodeTest.java
@@ -0,0 +1,28 @@
+package com.capgemini.linkedlist1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author ASUS
+ *
+ */
+public class MyNodeTest {
+
+	/**
+	 * 
+	 */
+	@Test
+	public void linkedListTest() {
+		MyNode<Integer> myFirstNode = new MyNode<>(56);
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		myFirstNode.setNext(mySecondNode);
+		mySecondNode.setNext(myThirdNode);
+		boolean result = myFirstNode.getNext().getNext().equals(myThirdNode);
+		Assert.assertTrue(result);
+	}
+
+}
